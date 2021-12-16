84002b240ef79a74e223d078c06cf9016d7f6b83
ThuanTha <tauan.silva00@gmail.com>
2021-12-16 16:22:39 -0300

    GET ID - TENTATIVA

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,5 @@
 <template>
-
-<!-- Alert Error -->
-    <div
-         class="bg-red-200 pl-6 py-3 rounded-md text-lg items-center w-96 absolute hidden"
-         >
-      <svg
-           viewBox="0 0 24 24"
-           class="text-red-600 w-5 h-5 sm:w-5 sm:h-5 mr-3"
-           >
-        <path
-              fill="currentColor"
-              d="M11.983,0a12.206,12.206,0,0,0-8.51,3.653A11.8,11.8,0,0,0,0,12.207,11.779,11.779,0,0,0,11.8,24h.214A12.111,12.111,0,0,0,24,11.791h0A11.766,11.766,0,0,0,11.983,0ZM10.5,16.542a1.476,1.476,0,0,1,1.449-1.53h.027a1.527,1.527,0,0,1,1.523,1.47,1.475,1.475,0,0,1-1.449,1.53h-.027A1.529,1.529,0,0,1,10.5,16.542ZM11,12.5v-6a1,1,0,0,1,2,0v6a1,1,0,1,1-2,0Z"
-              ></path>
-      </svg>
-      <span class="text-red-800"> Nenhum resultado foi encontrado. </span>
-    </div>
-    <!-- End Alert Error -->
-
-
-
-  <section class="absolute bg-cover w-full lg:w-screen h-screen" id="background">
+  <section class="flex bg-cover w-full lg:w-screen h-screen flex-col justify-start items-baseline" id="background">
     <br />
     <header id="header" class="bg-white w-10/12 h-10 rounded text-center ml-10 mt-1 pt-1 lg:w-11/12 lg:ml-16 lg:h-12  ">
       <h3  id="texto-header" class="text-xl font-sans lg:text-2xl"><b>BUSQUE POR UM ESTADO</b></h3>
@@ -664,6 +644,16 @@ export default {
 
       console.log(municipios);
     };
+
+    if(/Android [4-6]/.test(navigator.appVersion)) {
+   window.addEventListener("resize", function() {
+      if(document.activeElement.tagName=="INPUT" || document.activeElement.tagName=="TEXTAREA") {
+         window.setTimeout(function() {
+            document.activeElement.scrollIntoViewIfNeeded();
+         },0);
+      }
+   })
+}
 
     return {
       showModal,
